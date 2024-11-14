e368a66878a23b839f968181fa2690170074f37d
Bruno Oliveira <brunoroliveira7@gmail.com>
2024-11-14 12:15:28 -0300

    create item by JS

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
+const ul = document.querySelector("ul");
 
-form.submit = function (event) {
-    event.preventDefault();
-}
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (input.value === "") {
+    alert("Please enter a value.");
+    return false;
+  }
+  
+  const value = input.value;
+
+  ul.innerHTML += `<li class="item">
+  <span>${value}</span>
+  <button class="delete">X</button></li>`;
+
+});
